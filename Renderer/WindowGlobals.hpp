1a75ccd72474944e7da320b5c93a432b87ba8a81
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-09-01 20:01:05 +0900

    chore : 코드 일관성 수정

--- Renderer/WindowGlobals.hpp
+++ Renderer/WindowGlobals.hpp
@@ -8,4 +8,5 @@ struct WindowSize
 	
 namespace WindowGlobals {
 	inline WindowSize SCREENSIZE = { 384.0f, 1024.0f };
+
 }
--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// 1024 x 1024 크기에 윈도우 생성
 	HWND hWnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, WindowGlobals::SCREENSIZE.Width, WindowGlobals::SCREENSIZE.Height,
+		CW_USEDEFAULT, CW_USEDEFAULT, static_cast<int>(WindowGlobals::SCREENSIZE.Width), static_cast<int>(WindowGlobals::SCREENSIZE.Height),
 		nullptr, nullptr, hInstance, nullptr);
 
 	srand((UINT)GetTickCount64());
