90a2d51816d61286d0453c8ef1f51148a9ae2dc0
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-09-01 23:00:31 +0900

    Feat : Scene 기본 틀 생성

---
+++ Scene/GameScene.h
@@ -0,0 +1,51 @@
+﻿#pragma once 
+
+#include <vector>
+#include <memory>
+
+#include "IScene.hpp"
+
+#include "../Map/MapGlobals.hpp"
+
+#include "../Resource/Primitive.hpp"
+
+#include "../Physics/Collider.h"
+
+#include "../Player/Camera.h"
+
+class GameScene : public IScene {
+public:
+	GameScene() = default;
+	~GameScene() override = default;
+
+	GameScene(const GameScene&) = delete;
+	GameScene& operator=(const GameScene&) = delete;
+
+	GameScene(GameScene&&) = default;
+	GameScene& operator=(GameScene&&) = default;
+
+public:
+	virtual void Tick(float dt) override;
+	virtual void Render(IRenderer& renderer) override;
+
+	template <typename T, typename... Args>
+	inline bool CreatePrimitive(Args&&... args)
+	{
+		static_assert(std::is_base_of<Primitive, T>::value, "T must be derived from Primitive");
+		auto primitive = std::make_unique<T>(std::forward<Args>(args)...);
+		if (!primitive)
+		{
+			return false;
+		}
+		mPrimitives.emplace_back(std::move(primitive));
+		return true;
+	}
+
+private:
+	std::vector<std::unique_ptr<Primitive>> mPrimitives{};
+	
+	StaticCollider LeftWall{ Globals::LEFT_BORDER_POINT, Globals::LEFT_BORDER_NORMAL };
+	StaticCollider RightWall{ Globals::RIGHT_BORDER_POINT, Globals::RIGHT_BORDER_NORMAL };
+	StaticCollider TopWall{ Globals::TOP_BORDER_POINT, Globals::TOP_BORDER_NORMAL };
+	StaticCollider BottomWall{ Globals::BOTTOM_BORDER_POINT, Globals::BOTTOM_BORDER_NORMAL };
+};
---
+++ Scene/IScene.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once 
+#include "../Renderer/IRenderer.hpp"
+
+class IScene {
+public:
+	virtual  ~IScene() = default;
+
+public:
+	virtual void Tick(float dt) = 0; 
+	virtual void Render(IRenderer& renderer) = 0;
+
+	template<typename T>
+	T* As() {
+		return static_cast<T*>(this);
+	}
+};
