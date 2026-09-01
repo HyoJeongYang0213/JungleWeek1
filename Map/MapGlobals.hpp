2a1c39ead5c89e441d4ffe22465b72a770bbcb7f
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-09-01 20:48:13 +0900

    Feat : 창 크기와 맵 이미지 조정

--- Map/MapGlobals.hpp
+++ Map/MapGlobals.hpp
@@ -8,7 +8,7 @@ namespace Globals {
 
 	constexpr Vector3 LEFT_BORDER_POINT		= Vector3(0.0f, 0.0f, 0.0f);
 	constexpr Vector3 RIGHT_BORDER_POINT	= Vector3(15.0f, 0.0f, 0.0f);
-	constexpr Vector3 TOP_BORDER_POINT		= Vector3(0.0f, 40.0f, 0.0f);
+	constexpr Vector3 TOP_BORDER_POINT		= Vector3(0.0f, 30.0f, 0.0f);
 	constexpr Vector3 BOTTOM_BORDER_POINT	= Vector3(0.0f, 2.0f, 0.0f);
 
 	constexpr Vector3 LEFT_BORDER_NORMAL	= Vector3(1.0f, 0.0f, 0.0f);
@@ -19,7 +19,7 @@ namespace Globals {
 
 	constexpr float LEFT_BORDER = 0.0f;
 	constexpr float RIGHT_BORDER = 15.0f;
-	constexpr float TOP_BORDER = 40.0f;
+	constexpr float TOP_BORDER = 30.0f;
 	constexpr float BOTTOM_BORDER = 0.0f;
 
 	constexpr float MAP_WIDTH = 1536.0f;
--- Renderer/WindowGlobals.hpp
+++ Renderer/WindowGlobals.hpp
@@ -7,6 +7,6 @@ struct WindowSize
 };
 	
 namespace WindowGlobals {
-	inline WindowSize SCREENSIZE = { 384.0f, 1024.0f };
+	inline WindowSize SCREENSIZE = { 700.0f, 1400.0f };
 
 }
